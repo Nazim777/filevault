bddb123f8cfd7d325320395b7fdac02233cb5fe4
Nazim777 <iamnazimhossain@gmail.com>
2026-03-03 20:48:49 +0600

    feat: add client link

--- backend/src/app/middleware.ts
+++ backend/src/app/middleware.ts
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 // Cors Config ---
 const corsConfig: cors.CorsOptions = {
     credentials: true,
-    origin: 'http://localhost:3000', // Explicitly allow the frontend origin
+    origin: 'https://filevault-liard.vercel.app', // Explicitly allow the frontend origin
 };
 
 const middleware: any = [
